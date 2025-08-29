faec7c1fe788ad9a64879c3925880d2722f86b69
remediacourses-netizen <remedia.courses@gmail.com>
2025-08-30 04:23:25 +0500

    Update views.py

--- config/views.py
+++ config/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 from google_auth_oauthlib.flow import Flow
-from django.conf import settings
 import os
 
 def oauth2callback(request):
     code = request.GET.get('code')
-    state = request.GET.get('state')
-    
+
     flow = Flow.from_client_config(
         {
             "web": {
@@ -16,15 +14,20 @@ def oauth2callback(request):
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
                 "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
-                "redirect_uris": ["http://remedia.kz/oauth2callback"],
+                "redirect_uris": ["https://remedia.kz/oauth2callback/"],
             }
         },
         scopes=['https://www.googleapis.com/auth/drive.file']
     )
 
+    # 👇 ОБЯЗАТЕЛЬНО прописать redirect_uri
+    flow.redirect_uri = "https://remedia.kz/oauth2callback/"
+
+    # 👇 меняем код на токен
     flow.fetch_token(code=code)
 
     credentials = flow.credentials
     refresh_token = credentials.refresh_token
 
     return HttpResponse(f'✅ Refresh token получен: {refresh_token}')
+
